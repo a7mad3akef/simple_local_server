284192f76f7f464924533e5127e7b2d62891dd9b
a7mad3akef <a7mad3akef@gmail.com>
2018-03-12 22:26:47 +0200

    add another route

--- app.js
+++ app.js
@@ -10,11 +10,18 @@ app.use(bodyParser.json());
 //     console.log(request.query);
 //     response.send('hello');
 // });
+var intense = 50
 
 app.get('/lamp',function(req, res){
     // console.log(req.body)
-    res.json({'Brightness':50});  
+    res.json({Brightness:intense});  
 })
+
+app.get('/set', function(req, res){
+    console.log(req.query)
+    intense = parseInt(req.query.val)
+    res.json({Brightness:intense});
+});
 
 app.listen(4000, function () {
 	console.log('app running on port 4000')
